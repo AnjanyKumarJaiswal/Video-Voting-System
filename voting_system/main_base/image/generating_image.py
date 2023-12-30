953f5dbadf17339cb327a1fda79077de838a3e60
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2023-12-30 18:04:51 +0530

    null

--- voting_system/main_base/image/generating_image.py
+++ voting_system/main_base/image/generating_image.py
@@ -1,21 +1,2 @@
-import urllib.request
 
-# Creating a python file to generate Yotube links
-
-class Generate_links:
-    
-    def __init(self,ytlink,twichlink,videolink):
-        self.ytlink = ytlink
-        self.twichlink = twichlink
-        self.videolink = videolink
-    
-    def reqlink(request1 , request2):
-        # request = input()
-        response = urllib.request.urlopen(request1)
-        print()
-        response2 = urllib.request.URLopener(request2)
-        # data = response.read()
-        
-    reqlink(input("Enter the first url:"), input("Enter the second url:"))
-    
-    
+# Thie file contain for cre
