1ca78b46965ab358d8528489d12128decdb07683
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2023-12-30 11:17:19 +0530

    database setup

--- voting_system/main_base/main_base/settings.py
+++ voting_system/main_base/main_base/settings.py
@@ -75,10 +75,23 @@ WSGI_APPLICATION = 'main_base.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'video-voting',
+        'USER': 'anjany',
+        'PASSWORD': 'Mona@5322',
+        'HOST': 'video-voting.postgres.database.azure.com',
+        'PORT': '5432',
+        
+    },
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'CONNECTION_URL': 'postgres://anjany:Mona@5322@video-voting.postgres.database.azure.com:5432/video-voting',
+# }
 }
 
 
